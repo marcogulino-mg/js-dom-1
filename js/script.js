9eb7315e4715331b8bebc7dc03ff6bcf390a31aa
marco-gulino <marcogulino98@gmail.com>
2025-01-10 16:54:26 +0100

    [feat js]  img and button inner text can now revert to their previous state when ".btn-lamp" is pressed

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ const lampBtn = document.querySelector(".btn-lamp");
 const imgFrame = document.querySelector(".light-state");
 
 lampBtn.addEventListener("click", function () {
-  lampBtn.innerHTML = "Spegni";
-  imgFrame.src = "./img/yellow_lamp.png";
+  if(lampBtn.innerHTML == "Spegni") {
+    lampBtn.innerHTML = "Accendi";
+    imgFrame.src = "./img/white_lamp.png";
+  }
+  else {
+    lampBtn.innerHTML = "Spegni";
+    imgFrame.src = "./img/yellow_lamp.png";
+  }
 });
